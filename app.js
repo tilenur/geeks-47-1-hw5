bcdedb24bed1240759b473e80c8dca1725f1320b
Nurbek Tilebaliev <nurbektilebaliev@Nurbeks-MacBook-Air.local>
2025-02-28 02:08:40 +0900

    lesson 5 hw

---
+++ app.js
@@ -0,0 +1,9 @@
+var Красный = true;
+var Желтый = false;
+var Зеленый = false;
+
+if (Красный) console.log('Остановитесь!');
+else if (Желтый) console.log('Приготовьтесь!');
+else if (Зеленый) console.log('Можно ехать!');
+else console.log('Ошибка: все огни выключены.')
+
